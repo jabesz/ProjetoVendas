68c9b6bcfc6cffeefaf029c340d444ccb7de7f9b
jabes <jabesvieira4@gmail.com>
2024-05-21 22:01:48 -0300

    Vendas

--- ProjetoVendas/src/main/java/jdbc/ConnectionFactory.java
+++ ProjetoVendas/src/main/java/jdbc/ConnectionFactory.java
@@ -15,29 +15,7 @@ public class ConnectionFactory {
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/BDVENDAS";
     private static final String DATABASE_USERNAME = "root";
     private static final String DATABASE_PASSWORD = "jabes123";
-    private static final String SELECT_QUERY = "SELECT * FROM users WHERE username = ? AND password = ?";
     
-    public boolean validate(String username, String password) {
-
-            try {
-                Connection conn = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
-                PreparedStatement ps = conn.prepareStatement(SELECT_QUERY);
-                ps.setString(1, username);
-                ps.setString(2, password);
-
-                ResultSet rs = ps.executeQuery();
-
-                if (rs.next()) {
-                    return true;
-                }
-                
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                
-            return false;
-        }
-
         public Connection getConnection(){
             try{
                 Connection connection = DriverManager.getConnection(DATABASE_URL,DATABASE_USERNAME,DATABASE_PASSWORD);
